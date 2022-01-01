cfe41fe59a7fa3fec8efebd3d69f807fcf7e7ff1
xerx593 <alexlutz79@gmail.com>
2022-01-01 19:46:38 +0100

    improved solution

--- jpa/src/main/java/com/example/demo/AppConfig.java
+++ jpa/src/main/java/com/example/demo/AppConfig.java
@@ -3,24 +3,12 @@ package com.example.demo;
 import com.example.demo.domain.JpaConfig;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Configuration
 @PropertySource(value = "classpath:application.properties", ignoreResourceNotFound = true)
-@ComponentScan(
-        basePackageClasses = AppConfig.class,
-        useDefaultFilters = true,
-        excludeFilters = {
-                @ComponentScan.Filter(
-                        type = FilterType.ANNOTATION,
-                        classes = {Configuration.class, RestController.class, RestControllerAdvice.class}
-                )
-        }
-)
+@ComponentScan
 @Import(JpaConfig.class)
 public class AppConfig {
 }
--- jpa/src/main/java/com/example/demo/web/WebConfig.java
+++ jpa/src/main/java/com/example/demo/web/WebConfig.java
@@ -1,34 +1,16 @@
 package com.example.demo.web;
 
-import com.example.demo.AppConfig;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.List;
-import org.springframework.context.annotation.Import;
-
-@Configuration
 @EnableWebMvc
-@ComponentScan(
-    basePackageClasses = WebConfig.class,
-    useDefaultFilters = false,
-    includeFilters = {
-      @ComponentScan.Filter(
-          type = FilterType.ANNOTATION,
-          classes = {RestController.class, RestControllerAdvice.class}
-      )
-    }
-)
-@Import(AppConfig.class)
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
   @Autowired
