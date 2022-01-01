d36f4b7bcce7b033c9dd94ae95dec91f14ab9c84
xerx593 <alexlutz79@gmail.com>
2022-01-01 19:52:51 +0100

    clean more up

--- jpa/src/main/java/com/example/demo/AppConfig.java
+++ jpa/src/main/java/com/example/demo/AppConfig.java
@@ -1,14 +1,11 @@
 package com.example.demo;
 
-import com.example.demo.domain.JpaConfig;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource(value = "classpath:application.properties", ignoreResourceNotFound = true)
 @ComponentScan
-@Import(JpaConfig.class)
+@PropertySource(value = "classpath:application.properties", ignoreResourceNotFound = true)
 public class AppConfig {
 }
