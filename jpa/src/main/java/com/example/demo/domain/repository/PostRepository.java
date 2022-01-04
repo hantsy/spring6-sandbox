c7d2d5239f49d0f0db54eebb081155bcddacc099
hantsy <hantsy@gmail.com>
2022-01-04 20:18:20 +0800

    chore: clean codes

--- jpa/src/main/java/com/example/demo/domain/repository/PostRepository.java
+++ jpa/src/main/java/com/example/demo/domain/repository/PostRepository.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Stream;
 
+// To simplify the Repository interface definition, you can create `interface PostRepository extends Repository<Post, UUID>`
 public interface PostRepository {
     List<Post> findAll();
 
