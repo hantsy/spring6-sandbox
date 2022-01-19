359fd826d159bf354dc15beb000450219f1a3336
hantsy <hantsy@gmail.com>
2022-01-19 22:15:39 +0800

    chore: add querydsl to data-jpa

--- data-jpa/src/main/java/com/example/demo/domain/repository/PostRepository.java
+++ data-jpa/src/main/java/com/example/demo/domain/repository/PostRepository.java
@@ -3,10 +3,12 @@ package com.example.demo.domain.repository;
 import com.example.demo.domain.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import java.util.UUID;
 
 public interface PostRepository extends JpaRepository<Post, UUID>,
+        QuerydslPredicateExecutor<Post>,
         JpaSpecificationExecutor<Post>,
         PostRepositoryCustom {
 }
