9dea2e51667dbb6f2fa9c3d9680990370295fe69
hantsy <hantsy@gmail.com>
2022-03-26 15:32:48 +0800

    chore:clean codes

--- boot/src/main/java/com/example/demo/DemoApplication.java
+++ boot/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,6 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.AuditorAware;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-import java.util.Optional;
 
 @SpringBootApplication
 public class DemoApplication {
