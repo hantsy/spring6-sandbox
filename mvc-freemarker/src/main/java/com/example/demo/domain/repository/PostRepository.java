325302200ea021e50ea52f8fbfe88a50bb1cb361
hantsy <hantsy@gmail.com>
2022-03-26 18:51:30 +0800

    chore: clean codes

--- mvc-freemarker/src/main/java/com/example/demo/domain/repository/PostRepository.java
+++ mvc-freemarker/src/main/java/com/example/demo/domain/repository/PostRepository.java
@@ -4,12 +4,14 @@ import com.example.demo.domain.model.Post;
 import com.example.demo.domain.model.PostSummary;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 import java.util.UUID;
 
-public interface PostRepository extends PagingAndSortingRepository<Post, UUID> {
+public interface PostRepository extends PagingAndSortingRepository<Post, UUID>, ListCrudRepository<Post, UUID> {
     List<PostSummary> findBy();
+
     Page<PostSummary> findByTitleContains(String title, Pageable pageable);
 }
