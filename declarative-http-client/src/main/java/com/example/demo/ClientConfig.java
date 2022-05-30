9f392f03cbb6ead7012033260f308cc478fb4046
hantsy <hantsy@gmail.com>
2022-05-30 12:08:17 +0800

    chore: clean codes

--- declarative-http-client/src/main/java/com/example/demo/ClientConfig.java
+++ declarative-http-client/src/main/java/com/example/demo/ClientConfig.java
@@ -1,8 +1,11 @@
 package com.example.demo;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.support.DefaultFormattingConversionService;
+import org.springframework.http.codec.cbor.Jackson2CborDecoder;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -11,19 +14,25 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 public class ClientConfig {
 
     @Bean
-    WebClient webClient() {
+    WebClient webClient(ObjectMapper objectMapper) {
         return WebClient.builder()
                 .baseUrl("http://localhost:8080")
+                .codecs(clientCodecConfigurer -> {
+                            clientCodecConfigurer
+                                    .defaultCodecs()
+                                    .jackson2JsonDecoder(new Jackson2CborDecoder(objectMapper));
+                            clientCodecConfigurer
+                                    .defaultCodecs()
+                                    .jackson2JsonEncoder(new Jackson2JsonEncoder(objectMapper));
+                        }
+                )
                 .build();
     }
 
-
     @Bean
     PostClient postClient(WebClient webClient) {
-        var httpServiceProxyFactory = HttpServiceProxyFactory
-                .builder(new WebClientAdapter(webClient))
-                .setConversionService(new DefaultFormattingConversionService())
-                .build();
+        var httpServiceProxyFactory = new HttpServiceProxyFactory(new WebClientAdapter(webClient));
+        httpServiceProxyFactory.setConversionService(new DefaultFormattingConversionService());
         return httpServiceProxyFactory.createClient(PostClient.class);
     }
 
