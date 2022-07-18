9eea7449d8923eb64ffe09c83bb323fd857f445d
hantsy <hantsy@gmail.com>
2022-07-18 10:43:07 +0800

    chore: clean codes

--- declarative-http-client/src/main/java/com/example/demo/ClientConfig.java
+++ declarative-http-client/src/main/java/com/example/demo/ClientConfig.java
@@ -1,16 +1,16 @@
 package com.example.demo;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.SneakyThrows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.support.DefaultFormattingConversionService;
-import org.springframework.http.codec.cbor.Jackson2CborDecoder;
 import org.springframework.http.codec.json.Jackson2JsonDecoder;
 import org.springframework.http.codec.json.Jackson2JsonEncoder;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
-import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.SneakyThrows;
 
 @Configuration
 public class ClientConfig {
@@ -34,7 +34,7 @@ public class ClientConfig {
     @SneakyThrows
     @Bean
     PostClient postClient(WebClient webClient) {
-        var httpServiceProxyFactory = new HttpServiceProxyFactory(new WebClientAdapter(webClient));
+        var httpServiceProxyFactory = WebClientAdapter.createHttpServiceProxyFactory(webClient);
         httpServiceProxyFactory.setConversionService(new DefaultFormattingConversionService());
         httpServiceProxyFactory.afterPropertiesSet();
         return httpServiceProxyFactory.createClient(PostClient.class);
