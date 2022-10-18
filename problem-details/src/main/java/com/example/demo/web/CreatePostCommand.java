9d59db2f1a3081200ea1ce68d72b4c5cbe2005d4
hantsy <hantsy@gmail.com>
2022-10-18 23:08:42 +0800

    chore: clean codes

--- problem-details/src/main/java/com/example/demo/web/CreatePostCommand.java
+++ problem-details/src/main/java/com/example/demo/web/CreatePostCommand.java
@@ -2,7 +2,18 @@ package com.example.demo.web;
 
 import jakarta.validation.constraints.NotBlank;
 
+// bean validation is not supported on a record class
+// see: https://github.com/spring-projects/spring-framework/issues/27868
 public record CreatePostCommand(
         @NotBlank String title,
         @NotBlank String content) {
+    public CreatePostCommand {
+        if (title == null) {
+            throw new IllegalArgumentException("Title is required.");
+        }
+
+        if (content == null) {
+            throw new IllegalArgumentException("Content is required");
+        }
+    }
 }
