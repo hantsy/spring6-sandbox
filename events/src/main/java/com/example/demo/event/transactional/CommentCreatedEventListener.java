afd0b5608f48f2a04dc95e290ee3dc0860d799c6
hantsy <hantsy@gmail.com>
2022-10-22 14:41:21 +0800

    chore: clean codes

--- events/src/main/java/com/example/demo/event/transactional/CommentCreatedEventListener.java
+++ events/src/main/java/com/example/demo/event/transactional/CommentCreatedEventListener.java
@@ -3,14 +3,12 @@ package com.example.demo.event.transactional;
 import com.example.demo.domain.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
-import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 
 @Component
@@ -24,13 +22,16 @@ public class CommentCreatedEventListener {
     @Async
     public void onCommentCreated(CommentCreatedEvent event) {
         log.debug("on post created: {}", event);
+        //posts.increaseCommentsCount(event.eventData().getPostId().getId());
         this.eventStore.add(event);
     }
 
-
     @Scheduled(initialDelay = 3000L, fixedRate = 5000L)
     public void updateCommentsCount() {
-        eventStore.forEach(e -> posts.increaseCommentsCount(e.eventData().getPostId().getId()));
-        eventStore.clear();
+        for (var it = eventStore.iterator(); it.hasNext(); ) {
+            var event = it.next();
+            posts.increaseCommentsCount(event.eventData().getPostId().getId());
+            it.remove();
+        }
     }
 }
