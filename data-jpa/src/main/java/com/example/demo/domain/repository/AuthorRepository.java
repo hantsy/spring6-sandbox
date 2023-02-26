87cc2cc6a78441b01d5b52374af0561693fe30fc
hantsy <hantsy@gmail.com>
2023-02-26 14:09:54 +0800

    chore: clean codes

--- data-jpa/src/main/java/com/example/demo/domain/repository/AuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.domain.repository;
-
-import com.example.demo.domain.model.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface AuthorRepository extends JpaRepository<Author, UUID> {
-}
