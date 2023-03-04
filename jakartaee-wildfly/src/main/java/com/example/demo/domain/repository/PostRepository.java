6ae6a5a32ca5c8dac877922e8ed64080b3970191
hantsy <hantsy@gmail.com>
2023-03-04 15:46:38 +0800

    chore: clean codes

--- jakartaee-wildfly/src/main/java/com/example/demo/domain/repository/PostRepository.java
+++ jakartaee-wildfly/src/main/java/com/example/demo/domain/repository/PostRepository.java
@@ -2,13 +2,8 @@ package com.example.demo.domain.repository;
 
 import com.example.demo.domain.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import java.util.UUID;
 
-public interface PostRepository extends JpaRepository<Post, UUID>,
-        QuerydslPredicateExecutor<Post>,
-        JpaSpecificationExecutor<Post>,
-        PostRepositoryCustom {
+public interface PostRepository extends JpaRepository<Post, UUID>, PostRepositoryCustom {
 }
