2f8c723fcf9a67cf0043a559aa9bfee732806cc6
hantsy <hantsy@gmail.com>
2023-09-16 11:25:33 +0800

    chore: clean codes

--- data-jpa/src/test/java/com/example/demo/web/PostControllerTestWithMockMvcWebTestClient.java
+++ data-jpa/src/test/java/com/example/demo/web/PostControllerTestWithMockMvcWebTestClient.java
@@ -7,7 +7,6 @@ import com.example.demo.domain.repository.PostRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
@@ -18,7 +17,6 @@ import org.springframework.test.web.servlet.client.MockMvcWebTestClient;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 /**
@@ -66,6 +64,5 @@ public class PostControllerTestWithMockMvcWebTestClient {
         verify(this.posts, times(1)).findAll(isA(Specification.class), isA(Pageable.class));
         verifyNoMoreInteractions(this.posts);
     }
-
 
 }
