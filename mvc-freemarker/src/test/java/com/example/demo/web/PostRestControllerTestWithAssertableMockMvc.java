cf194da7caecb379191c6ddba1fb2c3c14535347
hantsy <hantsy@gmail.com>
2024-05-25 20:15:48 +0800

    chore: add assertj mockmvc example

---
+++ mvc-freemarker/src/test/java/com/example/demo/web/PostRestControllerTestWithAssertableMockMvc.java
@@ -0,0 +1,63 @@
+package com.example.demo.web;
+
+import com.example.demo.Jackson2ObjectMapperConfig;
+import com.example.demo.domain.model.Post;
+import com.example.demo.domain.repository.PostRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+/**
+ * @author hantsy
+ */
+@SpringJUnitWebConfig(classes = {WebConfig.class, Jackson2ObjectMapperConfig.class, TestDataConfig.class})
+public class PostRestControllerTestWithAssertableMockMvc {
+
+    @Autowired
+    WebApplicationContext ctx;
+
+    MockMvcTester mvc;
+
+    @Autowired
+    PostRepository posts;
+
+    @BeforeEach
+    public void setup() {
+        this.mvc = MockMvcTester.from(ctx);
+    }
+
+    @AfterEach
+    public void teardown() {
+        reset(this.posts);
+    }
+
+    @Test
+    public void getGetAllPosts() throws Exception {
+        when(this.posts.findAll())
+                .thenReturn(List.of(
+                                Post.of("test", "content of test1"),
+                                Post.of("test2", "content of test2")
+                        )
+                );
+
+        assertThat(this.mvc.perform(get("/api/posts").accept(MediaType.APPLICATION_JSON)))
+                .hasStatusOk()
+                .bodyJson().hasSize(2);
+        //       .has(new Condition<MvcTestResult>());
+
+        verify(this.posts, times(1)).findAll();
+        verifyNoMoreInteractions(this.posts);
+    }
+
+}
