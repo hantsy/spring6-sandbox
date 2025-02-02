11d8195dd0436aa673551d7ce0d3ca00f90a6618
hantsy <hantsy@gmail.com>
2025-02-02 17:13:44 +0800

    chore: clean codes

--- boot-vibur-dbcp/src/main/java/com/example/demo/ProductController.java
+++ boot-vibur-dbcp/src/main/java/com/example/demo/ProductController.java
@@ -1,2 +1,22 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    private final ProductRepository productRepository;
+
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping("")
+    public List<Product> productList() {
+        return this.productRepository.findAll();
+    }
+}
