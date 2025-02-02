79de429892a65488cbdceb1e98a7e8e8d4e39c17
hantsy <hantsy@gmail.com>
2025-02-02 19:06:55 +0800

    chore: clean codes

--- boot-vibur-dbcp/src/test/java/com/example/demo/TestcontainersConfiguration.java
+++ boot-vibur-dbcp/src/test/java/com/example/demo/TestcontainersConfiguration.java
@@ -12,7 +12,8 @@ class TestcontainersConfiguration {
 	@Bean
 	@ServiceConnection
 	PostgreSQLContainer<?> postgresContainer() {
-		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"))
+				.withExposedPorts(5432);
 	}
 
 }
