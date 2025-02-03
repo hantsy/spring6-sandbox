5b1dbe69c6a6a8c9b29e870ac503ec69f0461c4a
hantsy <hantsy@gmail.com>
2025-02-03 10:26:16 +0800

    chore: clean codes

--- boot-vibur-dbcp/src/main/java/com/example/demo/DataSourceConfig.java
+++ boot-vibur-dbcp/src/main/java/com/example/demo/DataSourceConfig.java
@@ -3,22 +3,26 @@ package com.example.demo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.lang.NonNull;
 import org.vibur.dbcp.ViburDBCPDataSource;
 
 import javax.sql.DataSource;
 
 
 @Configuration(proxyBeanMethods = false)
+@Profile({"default", "test"})
 public class DataSourceConfig {
     public static final Logger log = LoggerFactory.getLogger(DataSourceConfig.class);
 
-    @Bean(initMethod = "start", destroyMethod = "close")
-    @Profile({"default", "test"})
+    //@Bean(initMethod = "start", destroyMethod = "close")
+    @Bean
     public DataSource viburDataSource(DataSourceProperties dataSourceProperties) {
         log.debug("default dataSourceProperties: url={}, username={}, password={}, driverClassName={}",
                 dataSourceProperties.getUrl(),
@@ -32,5 +36,20 @@ public class DataSourceConfig {
                 .password(dataSourceProperties.getPassword())
                 .driverClassName(dataSourceProperties.getDriverClassName())
                 .build();
+    }
+
+    @Bean
+    public BeanPostProcessor viburDbcpDataSourceBeanPostProcessor() {
+        return new BeanPostProcessor() {
+            @Override
+            public Object postProcessAfterInitialization(
+                    @NonNull Object bean, @NonNull String beanName) throws BeansException {
+                if (bean instanceof ViburDBCPDataSource ds) {
+                    ds.start();
+                    return ds;
+                }
+                return bean;
+            }
+        };
     }
 }
