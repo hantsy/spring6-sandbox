414f3d39e9916c44d5854147e70ba5943aee006d
hantsy <hantsy@gmail.com>
2025-02-03 10:41:46 +0800

    chore: clean codes

--- boot-vibur-dbcp/src/main/java/com/example/demo/DataSourceConfig.java
+++ boot-vibur-dbcp/src/main/java/com/example/demo/DataSourceConfig.java
@@ -3,14 +3,11 @@ package com.example.demo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.lang.NonNull;
 import org.vibur.dbcp.ViburDBCPDataSource;
 
 import javax.sql.DataSource;
@@ -21,8 +18,7 @@ import javax.sql.DataSource;
 public class DataSourceConfig {
     public static final Logger log = LoggerFactory.getLogger(DataSourceConfig.class);
 
-    //@Bean(initMethod = "start", destroyMethod = "close")
-    @Bean
+    @Bean(initMethod = "start", destroyMethod = "close")
     public DataSource viburDataSource(DataSourceProperties dataSourceProperties) {
         log.debug("default dataSourceProperties: url={}, username={}, password={}, driverClassName={}",
                 dataSourceProperties.getUrl(),
@@ -36,20 +32,5 @@ public class DataSourceConfig {
                 .password(dataSourceProperties.getPassword())
                 .driverClassName(dataSourceProperties.getDriverClassName())
                 .build();
-    }
-
-    @Bean
-    public BeanPostProcessor viburDbcpDataSourceBeanPostProcessor() {
-        return new BeanPostProcessor() {
-            @Override
-            public Object postProcessAfterInitialization(
-                    @NonNull Object bean, @NonNull String beanName) throws BeansException {
-                if (bean instanceof ViburDBCPDataSource ds) {
-                    ds.start();
-                    return ds;
-                }
-                return bean;
-            }
-        };
     }
 }
