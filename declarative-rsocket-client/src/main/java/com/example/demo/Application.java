a8162013a7e3548b8af8d9f024d82732faa53b20
hantsy <hantsy@gmail.com>
2025-05-25 11:19:19 +0800

    chore: clean codes

--- declarative-rsocket-client/src/main/java/com/example/demo/Application.java
+++ declarative-rsocket-client/src/main/java/com/example/demo/Application.java
@@ -20,18 +20,14 @@ public class Application {
                 Application.class)) {
             var rSocketServer = context.getBean(RSocketServer.class);
             rSocketServer.bind(TcpServerTransport.create("localhost", 7000))
-                    .doOnTerminate(() -> System.out.println("Server is shutting down"))
                     .blockOptional()
                     .ifPresentOrElse(
                             c -> {
                                 System.out.println("Server is up on: " + c.address());
                                 try {
-                                 System.out.println("Enter q to exit");
-                                 var exit = (char)System.in.read();
-                                 if (exit == 'q') {
-                                     System.out.println("Exiting...");
-                                     System.exit(0);
-                                 }
+                                    System.out.println("Hint any key to exit:" + System.in.read());
+                                    System.out.println("Exiting...");
+                                    System.exit(0);
                                 } catch (IOException e) {
                                     throw new RuntimeException(e);
                                 }
